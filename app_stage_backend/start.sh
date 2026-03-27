28aa06aed7de32bab8b82bee170cbce368715638
Siham-Mrini <sihammerini1@gmail.com>
2026-03-27 08:40:08 +0000

    modification 2

--- app_stage_backend/start.sh
+++ app_stage_backend/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Script de démarrage pour Railway
+
+echo "--- Diagnostic du déploiement ---"
+php artisan config:clear
 php artisan migrate --force
 php artisan db:seed --force
+echo "--- Fin du diagnostic ---"
+
 php artisan serve --host=0.0.0.0 --port=$PORT
