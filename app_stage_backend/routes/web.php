3d759a38ee3d47d5a2bb55740dc79d53fe468767
Siham-Mrini <sihammerini1@gmail.com>
2026-03-30 09:06:22 +0000

    Fix login credentials and seeder names

--- app_stage_backend/routes/web.php
+++ app_stage_backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Root-level diagnostics for troubleshooting (signature v2.1)
+Route::get('health', [OrderController::class, 'health']);
+Route::get('check-db', [OrderController::class, 'checkDb']);
